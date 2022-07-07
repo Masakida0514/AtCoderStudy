af211cb83e4ddeacfe5af17a079ada55c36b5e65
Masakida0514 <masakida0514@akane.waseda.jp>
2022-07-07 12:36:34 +0900

    要復習

--- AtoCoder_Beginners_Selection/ABC086C_Traveling.cpp
+++ AtoCoder_Beginners_Selection/ABC086C_Traveling.cpp
@@ -3,30 +3,30 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep_1(i, n) for (int i = 1; i <= (int)(n); i++)
 
-static const int MAX = 1e4;
+static const int MAX = 1e5;
 
 
-int main() {
-  int N;
+void checker(int N, int x[], int y[], int t[]){
   int origin_x, origin_y;
   origin_x = origin_y = 0;
-  int t, x, y;
   int distance;
   int diff_t;
 
-  bool judge = false;
+  bool judge = true;
 
-  // 繰り返し部分
-  rep(i, N){
-    cin >> t >> x >> y;
+  rep(i,N){
+    if(i==0){
+      diff_t = 0;
+    } else {
+      diff_t = t[i] - t[i-1];
+    }
+    
+    distance = abs(x[i] - origin_x) + abs(y[i] - origin_y);
+    origin_x = x[i];
+    origin_y = y[i];
 
-    diff_t = t - diff_t;
-    distance = abs(x - origin_x) + abs(y - origin_y);
-
-    origin_x = x;
-    origin_y = y;
-  
     if(diff_t < distance){
+      judge = false;
       break;
     } else if (diff_t == distance){
       continue;
@@ -34,16 +34,16 @@ int main() {
       bool judge_2 = false;
       rep(m, diff_t){
         // t > x + y の時
-        if(diff_t - (x+y) % (2*m - 1) == 0){
+        if(diff_t - (x[i]+y[i]) % (2*m - 1) == 0){
           judge_2 = true;
           break;
         }
       }
       if(!judge_2){
+        judge = false;
         break;
       }
     }
-
   }
 
   if(judge){
@@ -51,6 +51,17 @@ int main() {
   } else {
     cout << "No" << endl;
   }
+}
 
-  
+
+int main() {
+  int N;
+  cin >> N;
+
+  int t[MAX], x[MAX],y[MAX];
+  rep(i,N){
+    cin >> t[i] >> x[i] >> y[i];
+  }
+
+  checker(N,x,y,t);
 }
