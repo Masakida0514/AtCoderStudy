ce0b01bf7a841fe3e148b4018967dc18180f6f6a
Masakida0514 <masakida0514@akane.waseda.jp>
2022-07-10 18:48:39 +0900

    DPむずかしいな

--- AtoCoder_Beginners_Selection/累積和/ABC098C.cpp
+++ AtoCoder_Beginners_Selection/累積和/ABC098C.cpp
@@ -9,19 +9,29 @@ typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
 
-static const int MAX = 1e2+100;
-int N, A[2][MAX], dp[2][MAX]; int res=0;
+static const int MAX = 3 * 1e5 + 100;
+int N, S[MAX],dp[MAX]; int res=0;
 bool judge = false;
 
 int main() {
     cin >> N;
-    rep_(y, 0, 2) rep_(x, 0, N) cin >> A[y][x];
- 
-    dp[0][0] = A[0][0];
-    rep_(y, 0, 2) rep_(x, 0, N) {
-        if (x) chmax(dp[y][x], dp[y][x - 1] + A[y][x]);
-        if (y) chmax(dp[y][x], dp[y - 1][x] + A[y][x]);
+    rep_(x, 0, N) cin >> S[x];
+
+    rep(i,N){
+        int ct = 0;
+        if(S[i]=='W'){
+            rep(j,i-1){
+                if(S[j]=='E') ct++;
+            }
+            rep_(k,i+1,N) if(S[k]=='W') ct++;
+        } else {
+            rep(j,i-1){
+                if(S[j]=='W') ct++;
+            }
+            rep_(k,i+1,N) if(S[k]=='E') ct++;
+        }
+        dp[i] = ct;
     }
- 
-    cout << dp[1][N - 1] << endl;
+
+    cout << max_element(dp,dp+MAX) << endl;
 }
